9b8cfa51a9a009fc45fe547debacfaeb74d7faac
Go7hic <gtfx0209@qq.com>
2020-11-09 22:48:46 +0800

    fix: bugfix

---
+++ src/Stone/LexerRunner.java
@@ -0,0 +1,9 @@
+package Stone;
+
+public class LexerRunner {
+	public static void main(String[] args) throws ParseException {
+		Lexer l = new Lexer(new CodeDialog());
+		for (Token t; (t = l.read()) != Token.EOF;)
+			System.out.println("=> " + t.getText());
+	}
+}
